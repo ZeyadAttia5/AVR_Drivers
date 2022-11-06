0e4509e8592bcdb6a3285990ec28fbc6cebd0f0b
Zeyad Mohamed Attia <85710632+ZeyadAttia5@users.noreply.github.com>
2022-11-07 00:50:02 +0200

    Add files via upload

---
+++ SPI/spi_prv.h
@@ -0,0 +1,17 @@
+/*
+ * spi_prv.h
+ *
+ *  Created on: Oct 15, 2022
+ *      Author: Administrator
+ */
+
+#ifndef INCLUDE_MCAL_SPI_SPI_PRV_H_
+#define INCLUDE_MCAL_SPI_SPI_PRV_H_
+#include "../../../SERVICES/std_types.h"
+
+#define SPCR *((volatile u8*) 0x2D)
+#define SPSR *((volatile u8*) 0x2E)
+#define SPDR *((volatile u8*) 0x2F)
+
+
+#endif /* INCLUDE_MCAL_SPI_SPI_PRV_H_ */
